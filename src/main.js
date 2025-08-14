4d2cea5d7b97e4c61525719398e0270f88ef31c1
dex <dexon.quantis@nonix.ai>
2025-08-14 23:26:35 +0200

    router to core/router

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import {createApp, h} from 'vue'
 import App from '@/App.vue'
-import {createRouter} from '@/router'
+import {createRouter} from 'src/core/router'
 
 import PrimeVue from 'primevue/config'
 import ToastService from 'primevue/toastservice'
