07ea160a1cedaf2565dc5fe1673225e0b6a08a53
dex <dexon.quantis@nonix.ai>
2025-08-15 00:38:30 +0200

    move teh stuff

--- vue_libs/nonix/page/DynamicPage.vue
+++ vue_libs/nonix/page/DynamicPage.vue
@@ -15,7 +15,7 @@ import DynamicWidgetList from '@nonix/dynamic-widget/DynamicWidgetList.vue'
 import Page from '@nonix/page/Page.vue'
 import { useRoute } from 'vue-router'
 import { inject, computed } from 'vue'
-import PageManager from '@nonix/page/PageManager.js'
+import PageManager from '@nonix/widget-manager/PageManager.js'
 
 const DynamicPage = {
   name: 'DynamicPage',
