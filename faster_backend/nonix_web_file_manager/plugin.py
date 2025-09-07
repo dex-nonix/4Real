2e9f6fa8e6edbefe5e6a0b6ba2d3310652eb6fe3
dex <dexon.quantis@nonix.ai>
2025-09-07 19:05:16 +0200

    Refactor plugin files to utilize decorators for service registration, removing manual DI registration methods for improved clarity and maintainability.

--- faster_backend/nonix_web_file_manager/plugin.py
+++ faster_backend/nonix_web_file_manager/plugin.py
@@ -1,11 +1,10 @@
-from nonix_web.plugin.base_plugin import BasePlugin, routers
-from nonix_web.utils.di import di_register
+from nonix_web.plugin.base_plugin import BasePlugin, routers, services
 from .routers.file import FileRouter
 from .routers.file_category import FileCategoryRouter
 from .routers.file_link import FileLinkRouter
-from .services.file_service import FileService
 from .services.file_category_service import FileCategoryService
 from .services.file_link_service import FileLinkService
+from .services.file_service import FileService
 
 
 @routers([
@@ -13,10 +12,10 @@ from .services.file_link_service import FileLinkService
     FileCategoryRouter,
     FileLinkRouter
 ])
+@services([
+    FileService,
+    FileCategoryService,
+    FileLinkService
+])
 class NxWebFileManagerPlugin(BasePlugin):
-
-    def _configure(self, server, config):
-        """Register services in DI system"""
-        di_register(FileService)
-        di_register(FileCategoryService)
-        di_register(FileLinkService)
+    pass
--- faster_backend/nonix_web_music_artist/plugin.py
+++ faster_backend/nonix_web_music_artist/plugin.py
@@ -1,12 +1,10 @@
-from nonix_web.plugin.base_plugin import BasePlugin, routers
-from nonix_web.utils.di import di_register
-
+from nonix_web.plugin.base_plugin import BasePlugin, routers, services
 from nonix_web_agentic.llm.llm_tools_decorator import llm_tools
 from .llm_tools.album_tools import album_tool_service
-from .llm_tools.track_tools import track_tool_service
 from .llm_tools.artist_tools import artist_tool_service
-from .llm_tools.style_tools import style_tool_service
 from .llm_tools.lyric_tools import lyric_tool_service
+from .llm_tools.style_tools import style_tool_service
+from .llm_tools.track_tools import track_tool_service
 from .routers.album import AlbumRouter
 from .routers.artist import ArtistRouter
 from .routers.rhyme_technique import RhymeTechniqueRouter
@@ -14,9 +12,9 @@ from .routers.style import StyleRouter
 from .routers.track import TrackRouter
 from .services.album_service import AlbumService
 from .services.artist_service import ArtistService
-from .services.track_service import TrackService
-from .services.style_service import StyleService
 from .services.rhyme_technique_service import RhymeTechniqueService
+from .services.style_service import StyleService
+from .services.track_service import TrackService
 
 
 @routers([
@@ -34,12 +32,13 @@ from .services.rhyme_technique_service import RhymeTechniqueService
     style_tool_service,  # Style CRUD + track assignment
     lyric_tool_service,  # Lyrics management
 ])
-class NxWebMusicArtistPlugin(BasePlugin):
+@services([
+    AlbumService,
+    ArtistService,
+    TrackService,
+    StyleService,
+    RhymeTechniqueService
 
-    def _configure(self, server, config):
-        """Register services in DI system"""
-        di_register(AlbumService)
-        di_register(ArtistService)
-        di_register(TrackService)
-        di_register(StyleService)
-        di_register(RhymeTechniqueService)
+])
+class NxWebMusicArtistPlugin(BasePlugin):
+    pass
