289b4113caa8afeb255715afa147b9fe0337556d
dex <dexon.quantis@nonix.ai>
2025-09-20 05:40:59 +0200

    ...

--- faster_backend/nonix_template/__init__.py
+++ faster_backend/nonix_template/__init__.py
@@ -1,5 +1,5 @@
 from .models.template import Template
-from .plugin import AsyncSessionLocal, get_db
+
 from .decorator import templates
 
-__all__ = ['Template', 'AsyncSessionLocal', 'get_db', 'templates']
+__all__ = ['Template', 'templates']
